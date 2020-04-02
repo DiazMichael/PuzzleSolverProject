dc1ec040f292351832f4055421bb3ff2279a3a1f
Mike Diaz <michael.diaz@mewssystems.com>
2020-04-02 11:02:41 +0200

    Add project files.

---
+++ ConsoleCore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var puzzle = new Puzzle();
+            
+
+        }
+
+        
+    }
+}
